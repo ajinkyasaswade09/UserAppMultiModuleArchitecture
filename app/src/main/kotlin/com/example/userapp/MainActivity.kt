248947522a517b5e559a31eee9306b48396a9289
ajinkyasaswade09 <ajinkyasaswade09@gmail.com>
2026-08-09 20:47:22 +0530

    Setup Jetpack Navigation and Hilt in MainActivity

--- app/src/main/kotlin/com/example/userapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/userapp/MainActivity.kt
@@ -5,25 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.ajinkya.users.navigation.UsersDestinations
+import com.ajinkya.users.navigation.usersGraph
 import com.example.userapp.ui.theme.UserAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             UserAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    UsersApp()
                 }
             }
         }
@@ -31,17 +35,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UserAppTheme {
-        Greeting("Android")
+fun UsersApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = UsersDestinations.LIST_ROUTE,
+    ) {
+        usersGraph(navController)
     }
 }
+
