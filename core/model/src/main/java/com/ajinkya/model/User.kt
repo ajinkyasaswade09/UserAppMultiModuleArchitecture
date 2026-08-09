da42d61f7dd68d5ba6573803d62480ea4c2ea709
ajinkyasaswade09 <ajinkyasaswade09@gmail.com>
2026-08-09 13:07:56 +0530

    Add core:model module and User data class

---
+++ core/model/src/main/java/com/ajinkya/model/User.kt
@@ -0,0 +1,16 @@
+package com.ajinkya.model
+
+data class User(
+    val id: Int,
+    val name: String,
+    val company: String,
+    val username: String,
+    val email: String,
+    val address: String,
+    val zip: String,
+    val state: String,
+    val country: String,
+    val phone: String,
+    val photo: String,
+)
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "UserAppMultiModuleArchitecture"
 include(":app")
+include(":core:model")
